d63c8021fef8b7956d2d82744b23d39c9802fd69
Angus ABWD <angusabwd@gmail.com>
2021-12-09 12:31:45 +0100

    Import avant la branche production

---
+++ Home/src/index.js
@@ -0,0 +1,12 @@
+if ( window.screen.availWidth < 768 ) {
+    document.getElementById("navbarContent").classList.add("collapse"); 
+}
+const collapse = () => {
+    if ( window.screen.availWidth < 768 ) {
+        document.getElementById("navbarContent").classList.add("collapse"); 
+    }
+    if ( window.screen.availWidth >= 768 ) {
+        document.getElementById("navbarContent").classList.remove("collapse"); 
+    }
+}
+window.onresize = collapse;
